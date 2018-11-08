f7538c8d495a4bfc32067e2ab71822dcfb259b33
arielzenprotocol <ariel@zenprotocol.com>
2018-11-08 18:40:48 +0200

    Rebased zen-desuger over next

--- build.fsx
+++ build.fsx
@@ -42,7 +42,6 @@ let runFStar args files =
 
   let executable,fstarPath,z3Path =
     if EnvironmentHelper.isLinux then ("mono", "packages/ZFStar/tools/fstar.exe", "packages/zen_z3_linux/output/z3-linux")
-    if EnvironmentHelper.isLinux then ("mono", "packages/ZFStar/tools/fstar.exe", "packages/zen_z3_linux/output/z3-linux")
     elif EnvironmentHelper.isMacOS then ("mono", "packages/ZFStar/tools/fstar.exe", "packages/zen_z3_osx/output/z3-osx")
     else ("packages/ZFStar/tools/fstar.exe","","packages/zen_z3_windows/output/z3.exe")
 
