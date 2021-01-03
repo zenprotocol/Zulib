93e10592fa6874df96f3cda9d9d90bf6cbadfb7e
Ariel Zen Protocol <ariel@zenprotocol.com>
2021-01-03 18:32:12 +0200

    test: Zen.Asset.parse

---
+++ tests/Zen.Asset.fsx
@@ -0,0 +1,30 @@
+#I "../.paket/load/net47"
+#r "../packages/BouncyCastle/lib/BouncyCastle.Crypto.dll"
+#r "../bin/Zulib.dll"
+
+open System
+open Zen.Types.Extracted
+open Zen.Types.Realized
+
+module C = Zen.Cost.Realized
+module P = Prims
+module Asset = Zen.Asset
+
+
+
+let test_asset_parse_zen =
+    
+    Asset.parse "00"B
+    |> C.__force
+    |> Option.get
+    |> fun asset ->
+        if asset <> Asset.zenAsset then
+            failwith "parsed asset wasn't Zen"
+    
+    Asset.parse "0000000000000000000000000000000000000000"B
+    |> C.__force
+    |> Option.get
+    |> fun asset ->
+        if asset <> Asset.zenAsset then
+            failwith "parsed asset wasn't Zen"
+
