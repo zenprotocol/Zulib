db348762adf6ac329bee16520be794d992ce80fe
arielzenprotocol <ariel@zenprotocol.com>
2021-09-12 16:52:47 +0300

    fix: __proj__Some__item__v didn't typecheck

--- fsharp/Realized/FStar.Pervasives.fs
+++ fsharp/Realized/FStar.Pervasives.fs
@@ -17,7 +17,7 @@ module Native =
 
     let uu___is_None = function None -> true | _ -> false
     let uu___is_Some = function Some _ -> true | _ -> false
-    let __proj__Some__item__v = function Some x -> x | _ -> assert false
+    let __proj__Some__item__v = function Some x -> x | _ -> failwith "impossible"
 
     type ('a,'b) tuple2 = 'a * 'b
 
